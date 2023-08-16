575c238a3db9bb16d317c4be3c29a8cb0fb6cd6d
Gustavo Wallace Macedo Santos <80765303+Gustavo-Wallace@users.noreply.github.com>
2023-08-16 10:56:55 -0300

    Add files via upload

---
+++ atividade2/variaveisOperadores.js
@@ -0,0 +1,20 @@
+var somaNum1 = 10;
+var somaNum2 = 10;
+var soma = somaNum1 + somaNum2;
+
+var subtracaoNum1 = 20;
+var subtracaoNum2 = 10;
+var subtracao = subtracaoNum1 - subtracaoNum2;
+
+var multiplicaoNum1 = 2;
+var multiplicaoNum2 = 5;
+var multiplicao = multiplicaoNum1 * multiplicaoNum2;
+
+var divisaoNum1 = 20;
+var divisaoNum2 = 2;
+var divisao = divisaoNum1 + divisaoNum2;
+
+console.log(`Resultado da soma entre ${somaNum1} e ${somaNum2} = ${soma}.`);
+console.log(`Resultado da subtração entre ${subtracaoNum1} e ${subtracaoNum2} = ${subtracao}.`);
+console.log(`Resultado da multiplicação entre ${multiplicaoNum1} e ${multiplicaoNum2} = ${multiplicao}.`);
+console.log(`Resultado da divisão entre ${divisaoNum1} e ${divisaoNum2} = ${divisao}.`);
