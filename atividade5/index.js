2793621e65e05c40b1674251fb0311f4f0082741
Gustavo Wallace Macedo Santos <80765303+Gustavo-Wallace@users.noreply.github.com>
2023-09-27 10:41:22 -0300

    Add files via upload

---
+++ atividade5/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const mustacheExpress = require('mustache-express');
+const app = express();
+
+app.engine('html', mustacheExpress());
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
+
+app.use(express.urlencoded({extended: true}));
+
+app.get('/', function(req, res) {
+    let usuario = {
+        nome: "Gustavo",
+        telefone: 1231313
+    };
+    res.render('index.html', {usuario});
+});
+
+app.post('/dados', function(req, res) {
+    let usuario = {
+        nome: req.body.nome,
+        telefone: req.body.telefone
+    };
+    res.render('dados.html', {usuario});
+});
+
+const PORT = 8080;
+app.listen(PORT, function () {
+    console.log('app rodando na porta ' + PORT);
+});
